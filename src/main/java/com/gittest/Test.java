2f7a8baad142056ca4de80d0d16c79369816b9af
Bear <Bear@test.cn>
2022-11-12 19:39:54 +0800

    master first commit

--- src/main/java/com/gittest/Test.java
+++ src/main/java/com/gittest/Test.java
@@ -5,5 +5,6 @@ public class Test {
         System.out.println("Hello Git");
         System.out.println("Hello Git");
         System.out.println("Hello Git333");
+        System.out.println("Hello Git master");
     }
 }
